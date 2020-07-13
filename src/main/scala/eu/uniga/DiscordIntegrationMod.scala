32ff0b89aa59e2337c6d73f33b5912f27683916b
Jiří Vrba <jiri@vrba.dev>
2020-07-13 08:10:55 +0200

    virgin Java vs chad Scala

---
+++ src/main/scala/eu/uniga/DiscordIntegrationMod.scala
@@ -0,0 +1,7 @@
+package eu.uniga
+
+import net.fabricmc.api.ModInitializer
+
+class DiscordIntegrationMod extends ModInitializer {
+  override def onInitialize(): Unit = println("Hello Fabric World!")
+}
