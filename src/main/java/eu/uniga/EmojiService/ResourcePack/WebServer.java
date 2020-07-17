eef2d51a7ec9dd4a20e05f6882163b79d847164c
Tomáš Kiec <kiectoma@fit.cvut.cz>
2020-07-17 20:13:16 +0200

    Error handling

--- src/main/java/eu/uniga/EmojiService/ResourcePack/WebServer.java
+++ src/main/java/eu/uniga/EmojiService/ResourcePack/WebServer.java
@@ -10,7 +10,7 @@ import java.io.*;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
 
-public class WebServer
+public final class WebServer
 {
 	private HttpServer _server;
 	private final Logger _logger = LoggerFactory.getLogger(this.getClass().getName());
@@ -23,7 +23,7 @@ public class WebServer
 		}
 		catch (IOException e)
 		{
-			_logger.error("Cannot start resource pack server", e);
+			_logger.warn("Cannot start resource pack server", e);
 		}
 		
 		_server.createContext("/resource-pack", new Handler());
@@ -42,21 +42,46 @@ public class WebServer
 	
 	static class Handler implements HttpHandler
 	{
+		private static final Logger _logger = LoggerFactory.getLogger(HttpHandler.class.getName());
+		
 		@Override
-		public void handle(HttpExchange exchange) throws IOException
+		public void handle(HttpExchange exchange)
 		{
 			if (!Files.exists(Service.ResourcePackLocation))
 			{
-				exchange.sendResponseHeaders(404, 0);
-				exchange.getResponseBody().close();
+				try
+				{
+					exchange.sendResponseHeaders(404, 0);
+					exchange.getResponseBody().close();
+				} catch (IOException e)
+				{
+					_logger.warn("Connection error", e);
+				}
 				
 				return;
 			}
 			
-			exchange.sendResponseHeaders(200, Files.size(Service.ResourcePackLocation));
 			OutputStream outputStream = exchange.getResponseBody();
-			Files.copy(Service.ResourcePackLocation, outputStream);
-			outputStream.close();
+			
+			try
+			{
+				exchange.sendResponseHeaders(200, Files.size(Service.ResourcePackLocation));
+				Files.copy(Service.ResourcePackLocation, outputStream);
+			}
+			catch (IOException e)
+			{
+				_logger.warn("Error reading resource pack", e);
+			}
+			
+			try
+			{
+				outputStream.close();
+			}
+			catch (IOException e)
+			{
+				_logger.warn("Connection error", e);
+			}
+			
 		}
 	}
 }
