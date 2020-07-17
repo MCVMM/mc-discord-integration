05a731969bbe0c9f25bc5df3ec54cd690aea20ae
Tomáš Kiec <kiectoma@fit.cvut.cz>
2020-07-17 20:12:38 +0200

    Added zip for resource pack

---
+++ src/main/java/eu/uniga/EmojiService/ResourcePack/Zip.java
@@ -0,0 +1,47 @@
+package eu.uniga.EmojiService.ResourcePack;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public final class Zip
+{
+	private final Logger _logger = LoggerFactory.getLogger(this.getClass().getName());
+	private final ZipOutputStream _stream;
+	
+	public Zip() throws IOException
+	{
+		Path tmpFile = Paths.get(Service.ResourcePackLocation.toString() + ".tmp");
+		
+		OutputStream stream = Files.newOutputStream(tmpFile, StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE);
+		_stream = new ZipOutputStream(stream);
+	}
+	
+	public void Add(String path, byte[] bytes) throws IOException
+	{
+		ZipEntry zipEntry = new ZipEntry(path);
+		_stream.putNextEntry(zipEntry);
+		_stream.write(bytes);
+		_stream.closeEntry();
+	}
+	
+	public void Finish() throws IOException
+	{
+		if (_stream == null)
+		{
+			_logger.warn("Closing already closed ZIP");
+			
+			return;
+		}
+		
+		_stream.close();
+	}
+}
