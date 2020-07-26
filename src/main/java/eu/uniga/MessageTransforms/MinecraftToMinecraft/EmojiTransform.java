8a0e3ffa630803436642244a2e52cb79cf66c045
Tomáš Kiec <kiectoma@fit.cvut.cz>
2020-07-26 04:53:19 +0200

    Improved short name parsing

--- src/main/java/eu/uniga/MessageTransforms/MinecraftToMinecraft/EmojiTransform.java
+++ src/main/java/eu/uniga/MessageTransforms/MinecraftToMinecraft/EmojiTransform.java
@@ -3,15 +3,18 @@ package eu.uniga.MessageTransforms.MinecraftToMinecraft;
 import eu.uniga.MessageTransforms.IMessageTransform;
 import eu.uniga.MessageTransforms.SurrogatePairsDictionary;
 import eu.uniga.Utils.Codepoints;
-import net.minecraft.text.TranslatableText;
+import eu.uniga.Utils.MinecraftStyle;
+import net.minecraft.text.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmojiTransform implements IMessageTransform
 {
-	private SurrogatePairsDictionary _emojiDictionary;
-	private static final Pattern _pattern = Pattern.compile("(<:[a-zA-Z\\-_]+?:\\d+?>+|:[a-zA-Z\\-_]+?:)");
+	private final SurrogatePairsDictionary _emojiDictionary;
+	private final Pattern _pattern = Pattern.compile("(<:[a-zA-Z0-9\\-_]+?:\\d+?>+|:[a-zA-Z0-9\\-_]+?:)");
 	
 	public EmojiTransform(SurrogatePairsDictionary emojiDictionary)
 	{
@@ -26,36 +29,69 @@ public class EmojiTransform implements IMessageTransform
 	
 	private TranslatableText TransformText(TranslatableText text)
 	{
-		for (int i = 0; i < text.getArgs().length; i++)
-		{
-			if (!(text.getArgs()[i] instanceof String)) continue;
-			
-			int lastIndex = 0;
-			String textPart = (String)(text.getArgs()[i]);
-			
-			Matcher matcher = _pattern.matcher(textPart);
-			StringBuilder buffer = new StringBuilder();
-			
-			while (matcher.find())
-			{
-				Integer found = _emojiDictionary.GetSurrogatePairFromShortName(matcher.group(1));
-				String append;
-				
-				if (found == null) append = matcher.group(1);
-				else append = Codepoints.Utf16ToString(found);
-				
-				buffer.append(textPart, lastIndex, matcher.start()).append(append);
-				lastIndex = matcher.end();
-			}
-			if (lastIndex < textPart.length())
-			{
-				buffer.append(textPart, lastIndex, textPart.length());
-			}
-			text.getArgs()[i] = buffer.toString();
-		}
-		
-		text.getSiblings().forEach(sibling -> TransformText((TranslatableText)sibling));
+		if (text.getArgs()[1] instanceof String) text.getArgs()[1] = TransformTextRec(new LiteralText((String)text.getArgs()[1]));
+		else if (text.getArgs()[1] instanceof MutableText) text.getArgs()[1] = TransformTextRec((MutableText)text.getArgs()[1]);
 		
 		return text;
+	}
+	
+	private MutableText TransformTextRec(MutableText text)
+	{
+		List<Text> oldSiblings = new ArrayList<>(text.getSiblings().size());
+		oldSiblings.addAll(text.getSiblings());
+		text.getSiblings().clear();
+		
+		LiteralText out;
+		
+		int lastIndex;
+		String textPart = text.getString();
+		
+		Matcher matcher = _pattern.matcher(textPart);
+		StringBuilder buffer = new StringBuilder();
+		
+		if (matcher.find())
+		{
+			out = new LiteralText(textPart.substring(0, matcher.start()));
+			
+			lastIndex = FoundMatch(out, matcher);
+		}
+		else
+		{
+			out = new LiteralText(textPart);
+			lastIndex = Integer.MAX_VALUE;
+		}
+		
+		while (matcher.find())
+		{
+			lastIndex = FoundMatch(out, matcher);
+		}
+		if (lastIndex < textPart.length())
+		{
+			out.append(new LiteralText(textPart.substring(lastIndex)));
+		}
+		
+		oldSiblings.forEach(sibling ->
+		{
+			if (sibling instanceof MutableText) out.append(TransformTextRec((MutableText)sibling));
+			else out.append(TransformTextRec(new LiteralText(sibling.getString())));
+		});
+		
+		return out;
+	}
+	
+	private int FoundMatch(LiteralText out, Matcher matcher)
+	{
+		int lastIndex;
+		Integer found = _emojiDictionary.GetSurrogatePairFromShortName(matcher.group(1));
+		String append;
+		
+		if (found == null) append = matcher.group(1);
+		else append = Codepoints.Utf16ToString(found);
+		
+		out.append(new LiteralText(append).setStyle(MinecraftStyle.NoStyle));
+		
+		lastIndex = matcher.end();
+		
+		return lastIndex;
 	}
 }
